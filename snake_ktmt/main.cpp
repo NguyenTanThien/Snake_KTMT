e6cdd68e2b49ed310e8436a6abad89324333cc04
SentoUIT <19521103@gm.uit.edu.vn>
2021-12-02 16:05:35 +0700

    UIT.19521103

--- snake_ktmt/main.cpp
+++ snake_ktmt/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+
+//Menu
+void Menu()
+{
+	int diff;
+	do
+	{
+		cout << "1.Lv1\t2.Lv2\t3.Lv3\t4.Lv4\t5.Lv5\nChon do kho: ";
+		cin >> diff;
+	} while (diff < 1 || diff>5);
+	switch (diff)
+	{
+	case 1:
+		speed *= 10;
+		break;
+	case 2:
+		speed *= 8;
+		break;
+	case 3:
+		speed *= 6;
+		break;
+	case 4:
+		speed *= 4;
+		break;
+	case 5:
+		speed *= 2;
+		break;
+	}
+	do
+	{
+		cout << "\n1.Che do co ban\t2.Che do tu do\nChon che do: ";
+		cin >> mode;
+	} while (mode < 1 || mode > 2);
+	cout << "Bat dau tro choi?\n";
+	system("pause");
+	system("cls");
+
+}
 int main()
 {
 	New_snake();
@@ -18,4 +56,5 @@ int main()
 	}
 	system("pause");
     return 0;
+    //19521103
 }
