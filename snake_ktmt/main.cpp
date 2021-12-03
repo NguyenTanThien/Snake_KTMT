fbb148556a4901a0a17c4923b9bc747b9c9319d5
UIT.19522165 <95128623+ThuongHieuTam@users.noreply.github.com>
2021-12-03 10:12:25 +0700

    UIT.19522165

--- snake_ktmt/main.cpp
+++ snake_ktmt/main.cpp
@@ -2,6 +2,19 @@
 #include <conio.h>
 #include <windows.h>
 
+using namespace std;
+
+const int width = 20;
+const int length = 20;
+bool end_game;
+int speed = 20;
+int mode;
+int x, y, score, food_X, food_Y;
+enum direction {Stop = 0, Left, Up, Right, Down};
+direction dir;
+int tail_X[200], tail_Y[200];
+int ntail;
+
 //Menu
 void Menu()
 {
@@ -39,6 +52,75 @@ void Menu()
 	system("cls");
 
 }
+
+//Rule
+void Rule()
+{
+	int x_sau = tail_X[0];
+	int y_sau = tail_Y[0];
+	int x2, y2;
+	tail_X[0] = x;
+	tail_Y[0] = y;
+	for (int i = 1; i < ntail; i++)
+	{
+		swap(tail_X[i], x_sau);
+		swap(tail_Y[i], y_sau);
+	}
+
+	switch (dir)
+	{
+	case Left:
+		x--;
+		break;
+	case Up:
+		y--;
+		break;
+	case Right:
+		x++;
+		break;
+	case Down:
+		y++;
+		break;
+	}
+
+	switch (mode)
+	{
+	case 1:
+	{
+		if (x >= length - 1 || x < 0)
+			end_game = true;
+		else if (y >= width - 1 || y < 1)
+			end_game = true;
+		break;
+	}
+	case 2:
+	{
+		if (x >= length - 1)
+			x = 0;
+		else if (x < 0)
+			x = length - 2;
+		if (y >= width - 1)
+			y = 1;
+		else if (y < 1)
+			y = width - 2;
+		break;
+	}
+	}
+
+	if (x == food_X && y == food_Y)
+	{
+		score += 10;
+		food_X = rand() % ((length - 2) - 2 + 1) + 2;
+		food_Y = rand() % ((width - 2) - 2 + 1) + 2;
+		ntail++;
+	}
+	for (int i = 0; i < ntail; i++)
+	{
+		if (x == tail_X[i] && y == tail_Y[i])
+			end_game = true;
+	}
+}
+
 int main()
 {
 	New_snake();
